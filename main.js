2633d066f905cf8034de45ab6dba0af975e01a5a
SillyPendejo <blinded@list.ru>
2022-06-07 16:53:43 +0300

    adds main.js and exports js classes

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Blog } from "./js/blog";
+import { Form } from "./js/form";
+
+const blog = new Blog(document.querySelector(".blog"));
+const form = new Form(document.querySelector(".checklist__form_element"));
